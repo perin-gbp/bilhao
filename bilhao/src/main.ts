c2d1f84c515626ecb82127d9d9d19fb0e321fb17
Arthur Perin <ext.arthur.perin@loopster.com.br>
2025-11-05 16:18:06 -0300

    cont desenv app

--- bilhao/src/main.ts
+++ bilhao/src/main.ts
@@ -4,6 +4,7 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
