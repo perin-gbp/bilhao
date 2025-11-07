1abb4e2cb6398a912307d96d2394497620f961e5
Arthur Perin <ext.arthur.perin@loopster.com.br>
2025-11-07 17:58:07 -0300

    versão final publi

--- bilhao/src/app/agradecimento/agradecimento.page.ts
+++ bilhao/src/app/agradecimento/agradecimento.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {
-  IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle,
+  IonCard, IonCardContent, IonCardHeader, IonCardTitle,
   IonContent, IonHeader, IonTitle, IonToolbar
 } from '@ionic/angular/standalone';
 
@@ -9,23 +9,9 @@ import {
   standalone: true,
   imports: [
     IonContent, IonHeader, IonToolbar, IonTitle,
-    IonCard, IonCardHeader, IonCardTitle, IonCardContent,
-    IonButton
+    IonCard, IonCardHeader, IonCardTitle, IonCardContent
   ],
   templateUrl: './agradecimento.page.html',
   styleUrls: ['./agradecimento.page.scss']
 })
-export class AgradecimentoPage {
-  async share() {
-    const url = 'https://perin-gbp.github.io/bilhao/';
-    const text = 'Acabei de checar meu 1 bilhão de segundos! ⏳';
-    try {
-      if ((navigator as any).share) {
-        await (navigator as any).share({ title: 'Bilhão de Segundos', text, url });
-      } else {
-        await navigator.clipboard.writeText(url);
-        alert('Link copiado! Cole onde quiser 😊');
-      }
-    } catch {}
-  }
-}
+export class AgradecimentoPage {}
